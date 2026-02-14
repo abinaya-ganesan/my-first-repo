984e0c61f9c294da0e8880b43b261094924bc022
Abinaya <abivishnu03@gmail.com>
2026-02-14 10:30:28 +0100

    Updated print code

--- file.py
+++ file.py
@@ -2,3 +2,5 @@ with open("notes.txt", "r") as file, open("notes2.txt", "w") as file2:
     for line in file:
         print(line)
         file2.write(line)
+
+print("I made it! I'm a millionaire now")
